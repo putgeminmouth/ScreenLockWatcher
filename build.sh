cc6b04835b885de3f7686012f049120f8b8da550
putgeminmouth <d>
2024-01-12 23:07:30 -0500

    build a tar file

--- build.sh
+++ build.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
 
 WORK=$(mktemp -d)
+RELEASE=$(mktemp -d)
 mkdir -p $WORK/build
 
 echo "WORK ${WORK}"
 
-# xcodebuild -list -project ScreenLockWatcher.xcodeproj
 xcodebuild archive -configuration Release -project ScreenLockWatcher.xcodeproj -scheme ScreenLockWatcher -archivePath $WORK/build
 
+OSVER=$(sw_vers | grep ProductVersion | awk '{print $2}')
+BINDIR=$WORK/build.xcarchive/Products/usr/local/bin
+BIN=ScreenLockWatcher
+OSBIN=${BIN}_macos_${OSVER}
+
+pushd $RELEASE
+
+cp $BINDIR/$BIN $BIN
+ln -s $BIN $OSBIN
+
+tar czvf ScreenLockWatcher.tar.gz *
+
+popd
+
 echo "WORK ${WORK}"
-echo "BIN $(ls $WORK/build.xcarchive/Products/usr/local/bin/ScreenLockWatcher)"
+echo "RELEASE ${RELEASE}"
+
+ls -l $RELEASE
