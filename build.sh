813bf242ac775c78855661f0b47d35d4049a4505
putgeminmouth <d>
2024-01-12 22:45:59 -0500

    build as Release

--- build.sh
+++ build.sh
@@ -6,6 +6,7 @@ mkdir -p $WORK/build
 echo "WORK ${WORK}"
 
 # xcodebuild -list -project ScreenLockWatcher.xcodeproj
-xcodebuild archive -project ScreenLockWatcher.xcodeproj -scheme ScreenLockWatcher -archivePath $WORK/build
+xcodebuild archive -configuration Release -project ScreenLockWatcher.xcodeproj -scheme ScreenLockWatcher -archivePath $WORK/build
 
 echo "WORK ${WORK}"
+echo "BIN $(ls $WORK/build.xcarchive/Products/usr/local/bin/ScreenLockWatcher)"
